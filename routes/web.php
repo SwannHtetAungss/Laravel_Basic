310ea0d4017c9bebbcd61030b2d7655dc530159b
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-11-06 21:58:16 +0630

    route to view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/products', function () {
+    return view('product');
+});
+
+Route::get('/shops', function () {
+    return view('shops.shop');
+});
+
+Route::get('/products/{product_id}/shops/{shop_id}', function ($product_id, $shop_id) {
+    return "This is Product page for product id " . $product_id . "shop id is " . $shop_id;
+});
