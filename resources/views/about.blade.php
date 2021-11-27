07f764e202440086fb7d0e00aa0af813b721749d
SwannHtetAung <swannhtetaung.ss@gmail.com>
2021-11-27 20:32:23 +0630

    url image,css,js

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,15 @@
 
 @section('title','About')
 
+@section('css')
+<link rel="stylesheet" href="{{asset('css/about.css')}}">
+@endsection
+
 @section('content')
-<h2>This is about.</h2>
+<h2 class="text">This is about.</h2>
+<img src="{{asset('image/aa.jpg')}}" alt="image">
+@endsection
+
+@section('js')
+<script src="{{asset('js/about.js')}}"></script>
 @endsection
